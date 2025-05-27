81f7ad7ad180f4b29554f68c92fce99df7356e5e
Ruffy93 <rafael-neumann@web.de>
2025-05-27 11:42:18 +0200

    aufgabge 3.2.6

---
+++ Aufgabe 3.2.5.py
@@ -0,0 +1,4 @@
+counter = 5
+while counter < 10:
+    print("hier steht code, der wiederholt ausgeführt werden soll")
+    counter +=1
---
+++ Aufgabe 3.2.6/Experte1.py
@@ -0,0 +1,24 @@
+import random
+
+zahl = random.randint(1, 100)
+versuche = 5
+
+print("finde die richtige Zahl zwischen 0 und 100")
+print(f"Du hast {versuche} Versuche, enttäusch mich nicht!")
+
+while True:
+    tipp = int(input("Deine Zahl:"))
+    versuche -= 0
+
+    if tipp == zahl:
+        print(f"DU HAST DIE RICHTIGE ZAHL, LUCKY BOYYYYY! Versuche: {versuche}")
+        break
+    elif tipp < zahl:
+        print("Deine Zahl ist zu niedrig")
+    else:
+        print("Deine Zahl ist zu Hoch")
+
+    if versuche == 0:
+        print(f"Verloren!!!!!")
+    else:
+        print(f"Noch {versuche} Versuche übrig")   
